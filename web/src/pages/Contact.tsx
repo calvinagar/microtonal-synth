3c0e7e091943bdcb12116a4c0e842f58c7d8fe99
Calvin Agar <81779445+calvinagar@users.noreply.github.com>
2023-10-31 19:15:27 -0400

    Update Contact.tsx

--- web/src/pages/Contact.tsx
+++ web/src/pages/Contact.tsx
@@ -6,18 +6,11 @@ export function Contact() {
         <div className={"w-full p-10"}>
         <BorderedContainer className={"flex justify-center px-8 py-8 text-left text-base"}>
         <article className={"prose prose-white prose-invert"}>
-        If you have any questions or suggestions about the project, please feel free to contact us
-        through the email addresses below.
+        If you have any questions or suggestions about the project, please feel free to contact me
+        through the email address below.
         <br></br>
         <br></br>
-        Dr. Ayers:
-        <br></br>
-        William.Ayers@ucf.edu
-        <br></br>
-        <br></br>
-        Dr. Leinecker:
-        <br></br>
-        Richard.Leinecker@ucf.edu
+        calvinlagar@gmail.com
         </article>
         </BorderedContainer>
         </div>
