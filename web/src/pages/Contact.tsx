8b951c5faac7a9afddf9ffe29d5f7d30e83265df
Calvin Agar <calvin.agar@datably.io>
2023-11-13 23:55:14 -0500

    clean up display pages

--- web/src/pages/Contact.tsx
+++ web/src/pages/Contact.tsx
@@ -1,18 +1,24 @@
-import * as React from "react";
-import BorderedContainer from '../ui/BorderedContainer'
+import * as React from 'react';
+import BorderedContainer from '../ui/BorderedContainer';
 
 export function Contact() {
     return(
-        <div className={"w-full p-10"}>
-        <BorderedContainer className={"flex justify-center px-8 py-8 text-left text-base"}>
-        <article className={"prose prose-white prose-invert"}>
-        If you have any questions or suggestions about the project, please feel free to contact me
-        through the email address below.
-        <br></br>
-        <br></br>
-        calvinlagar@gmail.com
-        </article>
-        </BorderedContainer>
+        <div className={'w-full p-10'}>
+            <BorderedContainer className={'flex justify-center px-8 py-8 text-left text-base'}>
+                <article className={'prose prose-white prose-invert'}>
+                    If you have any questions or suggestions about the project, feel free to open any issues on
+                    github, or you can contact me through my email address.
+                    <br></br>
+                    <a 
+                        href={'https://github.com/calvinagar/microtonal-synth'}
+                        className={'font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline'}
+                    >
+                        https://github.com/calvinagar/microtonal-synth
+                    </a>
+                    <br></br>
+                    calvinlagar@gmail.com
+                </article>
+            </BorderedContainer>
         </div>
     )
 }
--- web/src/pages/Download.tsx
+++ web/src/pages/Download.tsx
@@ -1,20 +1,34 @@
-import * as React from "react";
+import * as React from 'react';
 import BorderedContainer from '../ui/BorderedContainer'
 
 export function Download() {
     return (
-        <div className={"w-full p-10"}>
-        <BorderedContainer className={"flex justify-center px-8 py-8 text-left text-base"}>
-        <article className={"prose prose-white prose-invert"}>
-        The most recent release of the desktop synthesizer and the source code for both the desktop 
-        and browser synthesizer projects from which this project was forked from can be found at the link below.
-        <br></br>
-        <br></br>
-        <a href="https://github.com/microtonality" 
-        className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">
-            https://github.com/microtonality</a>
-        </article>
-        </BorderedContainer>
+        <div className={'w-full p-10'}>
+            <BorderedContainer className={'flex justify-center px-8 py-8 text-left text-base'}>
+                <article className={'prose prose-white prose-invert'}>
+                    The source code for browser synthesizer project 
+                    from which this project was forked can be found at the link below.
+                    <br></br>
+                    <a 
+                        href={'https://github.com/Microtonality/Microtonality'}
+                        className={'font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline'}
+                    >
+                        https://github.com/Microtonality/Microtonality
+                    </a>
+
+                    <br></br>
+                    <br></br>
+                    
+                    You can also check out the official browser version here!
+                    <br></br>
+                    <a 
+                        href={'https://www.microtonality.net'}
+                        className={'font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline'}
+                    >
+                        https://www.microtonality.net
+                    </a>
+                </article>
+            </BorderedContainer>
         </div>
     )
 }
