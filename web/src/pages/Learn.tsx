500e060f3a346022f18aa1b1a5ca2343ad0fa4d3
Calvin Agar <calvin.agar@datably.io>
2023-11-14 20:40:34 -0500

    Text updates

--- web/src/pages/Learn.tsx
+++ web/src/pages/Learn.tsx
@@ -5,105 +5,102 @@ export function Learn() {
     return (
         <div className={'w-full p-10'}>
             <BorderedContainer className={'flex justify-center px-8 py-8 text-left text-base'}>
-                <article className={'prose prose-white prose-invert'}>
+                <article className={'prose prose-invert'}>
                     This project works on multiple fronts to bring microtonal works to life. As a starting point,
                     Easley Blackwood's microtonal etudes provide a number of difficulties to overcome in order to
                     produce a full performance. You can read about the ideas to tackle these challenges in the
                     paper below.
 
-                    <br></br>
-                    <br></br>
+                    <br/>
+                    <br/>
 
                     Richard Leinecker and William R. Ayers, “Performing Easley Blackwood's 
                     <a className={'italic ...'}> Twelve Microtonal Etudes</a>
-                    : An Open-Source Software Development Project,” in 
+                    : An Open-Source Software Development Project”, in 
                     <a className={'italic ...'}> Mathematics and Computation in Music 2022</a>
-                    , edited by Mariana Montiel, Octavio A. Agustín-Aquino, Francisco Gómez, Jeremy Kastine, Emilio Lluis-Puebla,
-                    and Brent Milam (2022): 331-339. Springer Nature.
-                    
-                    <br></br>
-                    
+                    , edited by Mariana Montiel, Octavio A. Agustín-Aquino, Francisco Gómez, 
+                    Jeremy Kastine, Emilio Lluis-Puebla, and Brent Milam (2022): 331-339. Springer Nature.
                     <a 
                         href={'https://doi.org/10.1007/978-3-031-07015-0_27'}
-                        className={'font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline'}
+                        className={'ml-1 font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline'}
                     >
                         https://doi.org/10.1007/978-3-031-07015-0_27
                     </a>
                 </article>
             </BorderedContainer>
 
-            <br></br>
-            <br></br>
+            <br/>
+            <br/>
 
             <BorderedContainer className={'flex justify-center px-8 py-8 text-left text-base'}>
-                <article className={'prose prose-white prose-invert'}>
+                <article className={'prose prose-invert'}>
                     This browser application was produced by Computer Science majors
                     completing their Senior Design Projects at the University of Central Florida (listed below).
                     The synthesizer includes microtonal scale generation, tone generators, ADSR envelope controls, 
                     keyboard mapping functionality, and components for saving mappings and instruments.
 
-                    <br></br>
-                    <br></br>
+                    <br/>
+                    <br/>
                     
                     Austin Smith
-                    <br></br>
+                    <br/>
                     
                     Peter Devita
-                    <br></br>
+                    <br/>
                     
                     Noah Hartley
-                    <br></br>
+                    <br/>
                     
                     Zachary Hasley
-                    <br></br>
+                    <br/>
                     
                     Nathan Manierre
-                    <br></br>
+                    <br/>
                     
                     Calvin Agar
                 </article>
             </BorderedContainer>
 
-            <br></br>
-            <br></br>
+            <br/>
+            <br/>
 
             <BorderedContainer className={'flex justify-center px-8 py-8 text-left text-base'}>
-                <article className={'prose prose-white prose-invert'}>
-                    The microtonal synthesizer desktop application was produced by Computer
-                    Science majors completing their Senior Design Projects at the University of Central Florida
+                <article className={'prose prose-invert'}>
+                    The microtonal synthesizer desktop application that preceded the browser version was produced by 
+                    Computer Science majors completing their Senior Design Projects at the University of Central Florida
                     (listed below). 
                     
-                    <br></br>
-                    <br></br>
+                    <br/>
+                    <br/>
                     
                     Jake DiClemente
-                    <br></br>
+                    <br/>
                     
                     Isaiah Kovacich
-                    <br></br>
+                    <br/>
                     
                     Anthony Moore
-                    <br></br>
+                    <br/>
                     
                     Michael Ortiz
-                    <br></br>
+                    <br/>
                     
                     John Pham
                 </article>
             </BorderedContainer>
 
-            <br></br>
-            <br></br>
+            <br/>
+            <br/>
         
             <BorderedContainer className={'flex justify-center px-8 py-8 text-left text-base'}>
-                <article className={'prose prose-white prose-invert'}>
+                <article className={'prose prose-invert'}>
                     This project also engages students in the School of Performing Arts at the University of Central
                     Florida (listed below). Music students work on notation projects to convert microtonal notation
                     into easily readable, comprehensible, and gesturally practicable notation for the purpose of
                     performance on the microtonal synthesizer using a standard keyboard controller.
                     
-                    <br></br>
-                    <br></br>
+                    <br/>
+                    <br/>
                     
                     Michael Crisafi
                 </article>
--- web/src/pages/Splash.tsx
+++ web/src/pages/Splash.tsx
@@ -5,20 +5,24 @@ export function Splash() {
     return (
         <div className={'w-full p-10'}>
             <BorderedContainer className={'flex justify-center px-8 py-8 text-left text-base'}>
-                <article className={'prose prose-white prose-invert'}>
+                <article className={'prose prose-invert'}>
                     This open-source development project is dedicated to the performance of microtonal music.
                     The project incorporates multiple elements to this effect, including a standalone desktop
                     software synthesizer and a browser application that allow microtonal playback. This project
                     began as an effort to fulfill Easley Blackwood's wish to hear a live performance of his Twelve
                     Microtonal Etudes (1980), but the project also has the potential to inspire performances of
                     other existing microtonal works and the creation of new works.
+                    
+                    <br/>
+                    <br/>
+                    
                     On this website, you can learn about the project, its elements, and the team members under the Learn tab,
                     you can test out some elements of microtonality in your browser under the Play tab, and you can access the
                     source code to different elements of the project, including the desktop and browser
-                    applications under the Download tab. Feel free to get in touch with us if you have any issues or inquiries.
-                    Our information can be found under the Contact tab.
+                    applications under the Download tab.
 
-                    <br></br>
+                    <br/>
+                    <br/>
                     
                     Richard Leinecker and William R. Ayers, University of Central Florida
                 </article>
